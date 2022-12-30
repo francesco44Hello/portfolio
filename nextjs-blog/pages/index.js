1b681c61574c11cbedcd2d2c49d4ca9d33960941
francesco44Hello <francescolongo44@gmail.com>
2022-12-30 13:14:45 +0000

    fixed import layout

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
-import Layout, { siteTitle } from "../components/layout";
+import Layout, { siteTitle } from "../components/layout/layout";
 import utilStyles from "../styles/utils.module.css";
 
 
