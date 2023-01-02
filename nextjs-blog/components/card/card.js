dfdc07a0dad56cd37f4545951fd091434880c239
francesco44Hello <francescolongo44@gmail.com>
2023-01-02 20:39:45 +0000

    modified styling of projects cards

--- nextjs-blog/components/card/card.js
+++ nextjs-blog/components/card/card.js
@@ -2,15 +2,21 @@ import React from 'react'
 import CardButton from '../cardbutton/cardbutton'
 import { snapvoteClick } from '../functions/functions'
 const Card = ({title,onClick, handleRepo,img }) => {
+    
     return (
         <div className='card'>
-            <h1>{title}</h1>
-            <div className='card-img-div'>
+            <>
             <img src={img}></img>
-            </div>
+            </>
+            <div className='bottom-card'>
+            <h2>{title}</h2>
             <div className='button-container'>
+            
+            
             <CardButton className='button' onClick={onClick} onClickRepo={handleRepo}>Visit me</CardButton>
             </div>
+            </div>
+            
            
         </div>
     )
