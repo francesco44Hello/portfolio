228d5b3acbb03c692bf35af772b87fccda6e5ee8
francesco44Hello <francescolongo44@gmail.com>
2023-01-27 20:57:04 +0100

    added css animation on hover to links in navbar

--- nextjs-blog/components/layout/layout.js
+++ nextjs-blog/components/layout/layout.js
@@ -1,19 +1,21 @@
 import NavLink from "next/link";
+import Link from "next/link";
 import Button from "../button/button.js";
 import { buttonClick } from "../functions/functions.js";
 import Image from 'next/image'
+// import Link from "next/link.js";
 
 export default function Layout() {
   return (
     <div className="navbar">
-    <NavLink    style={{ textDecoration: "none" }}
+    <Link    style={{ textDecoration: "none" }}
           // activeStyle={{ color: "red" }}
           // className="links"
           href="/" >
       <img src='https://i.ibb.co/NrP5ZDM/portfolio-Logo.png' alt='logo' 
       style={{ width: '6rem', height: '4.5rem'}} ></img>
     {/* <Image  src='https://i.ibb.co/T2SqB7f/logo-color.png' width={90} height={70}/> */}
-    </NavLink>
+    </Link>
    
       <div className="navbar-elements">
         {/* <NavLink
@@ -24,30 +26,30 @@ export default function Layout() {
         >
           Home
         </NavLink> */}
-        <NavLink
+        <Link
           style={{ textDecoration: "none" }}
-          activeclassname="active"
+          activeClassName="active"
           className="links"
           href="/about"
         >
           About
-        </NavLink>
-        <NavLink
+        </Link>
+        <Link
           style={{ textDecoration: "none" }}
-          activeclassname="active"
+          activeClassName="active"
           className="links"
           href="/projects"
         >
           Projects
-        </NavLink>
-        <NavLink
+        </Link>
+        <Link
           style={{ textDecoration: "none" }}
-          activeclassname="active"
+          activeClassName="active"
           className="links"
           href="/contact"
         >
           Contacts
-        </NavLink>
+        </Link>
         <Button className="links"  handleClick={buttonClick}/>
       </div>
     </div>
