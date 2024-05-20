e7d3a6ecd0d7421c50c9696f329347b11a6d949f
francesco44Hello <francescolongo44@gmail.com>
2024-05-20 09:31:28 +0100

    Modified About me Section

--- nextjs-blog/components/about/about.js
+++ nextjs-blog/components/about/about.js
@@ -6,35 +6,36 @@ const About = () => {
       <div className={styles.mainAboutDiv}>
         <div className={styles.aboutText}>
           <h1>About me</h1>
+
           <p>
             I am a passionate developer who is always eager to learn and grow in
             the field of technology. Four years ago, I came to the UK with the
             goal of learning English, but I quickly realized that my true
             passion lay in pursuing a{" "}
-            <span style={{ color: "#00ADB5" }}>career in tech. </span>After
-            moving to Liverpool, I enrolled in a coding bootcamp called School
-            of Code, where I gained hands-on experience in coding and software
-            development.
+            <span style={{ color: "#00ADB5" }}>career in tech.</span> After
+            gaining foundational skills at a coding bootcamp called School of
+            Code, I embarked on a professional journey in software development.
           </p>
           <p>
-            The bootcamp not only helped me acquire technical skills in{" "}
-            <span style={{ color: "#00ADB5" }}>front-end and back-end</span>{" "}
-            development, but also equipped me with the essential soft skills
-            required to work collaboratively in teams and communicate
-            effectively with clients. Since then, I have worked on a range of
-            web and mobile applications, both as a solo developer and as part of
-            a team. Through these projects, I have gained invaluable experience
-            in building{" "}
-            <span style={{ color: "#00ADB5" }}>scalable and efficient </span>
-            web and mobile applications that meet the needs of clients and
-            end-users.
+            Over the past year, I have honed my skills at a startup called UNIS,
+            where I worked extensively with{" "}
+            <span style={{ color: "#00ADB5" }}>Next.js, React, </span> and
+            <span style={{ color: "#00ADB5" }}> Firebase</span> , and also gained
+            experience with{" "}
+            <span style={{ color: "#00ADB5" }}>React Native</span>. At UNIS, I
+            played a key role in developing and maintaining scalable and
+            efficient web and mobile applications, contributing significantly to
+            various projects and collaborating closely with cross-functional
+            teams.
           </p>
           <p>
-            As a developer, I believe that learning{" "}
-            <span style={{ color: "#00ADB5" }}>never stops.</span> I am always
-            eager to stay up-to-date with the latest technologies and trends,
-            and I strive to continuously improve my skills and knowledge in
-            software development. I am excited to continue growing and
+            Unfortunately, due to redundancy, I am now seeking new
+            opportunities. Despite this setback, I remain committed to
+            continuous learning and growth. As a developer, I believe that
+            learning <span style={{ color: "#00ADB5" }}>never stops.</span> I am
+            always eager to stay up-to-date with the latest technologies and
+            trends, and I strive to continuously improve my skills and knowledge
+            in software development. I am excited to continue growing and
             contributing to the tech community in meaningful ways, and I am open
             to new challenges and opportunities.
           </p>
